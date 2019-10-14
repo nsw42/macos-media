544f661d6931bf3b41899635e2180279f0954d91
Neil Walker <neil@wynded.co.uk>
2019-10-14 22:59:29 +0100

    Update packaging info

---
+++ macos-media/__init__.py
@@ -0,0 +1,1 @@
+name = 'macos-media'
--- setup.py
+++ setup.py
@@ -1,4 +1,21 @@
-from distutils.core import setup
-setup(name='macospodcasts',
-      version='1.0',
-      py_modules=['macospodcasts'])
+import setuptools
+
+with open("README.md") as handle:
+    long_description = handle.read()
+
+setuptools.setup(
+    name='macos-media',
+    version='1.0.0',
+    author='Neil Walker',
+    author_email='neil@wynded.co.uk',
+    description='Access to the macOS media libraries',
+    long_description=long_description,
+    url='https://github.com/nsw42/macos-media',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS'
+    ],
+    python_requires='>=3.6',
+)
