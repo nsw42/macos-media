f218b1641f8e13fccf4493a95a29533e49ab9ba1
Neil Walker <neil@wynded.co.uk>
2019-12-23 14:30:47 +0000

    Add a link from Episode to Podcast

--- macos_media/podcasts.py
+++ macos_media/podcasts.py
@@ -15,13 +15,14 @@ Podcast.title.__doc__ = 'The title of the podcast show (str)'
 Podcast.podcast_id.__doc__ = 'The unique identifier of this podcast id (int)'
 
 
-Episode = namedtuple('Episode', ('title', 'publication_date', 'playcount', 'filepath', 'uuid'))
+Episode = namedtuple('Episode', ('title', 'publication_date', 'playcount', 'filepath', 'uuid', 'podcast'))
 Episode.__doc__ = 'Information about a single episode from a podcast'
 Episode.title.__doc__ = 'The title of this episode (str)'
 Episode.publication_date.__doc__ = 'The date that the episode was released (datetime.date)'
 Episode.playcount.__doc__ = 'The number of times the episode has been played (int)'
 Episode.filepath.__doc__ = 'The absolute path to the episode file (pathlib.Path)'
 Episode.uuid.__doc__ = 'The UUID of this episode (str)'
+Episode.podcast.__doc__ = 'The Podcast object to which this episode relates'
 
 
 def _convert_pubdate(pubdate):
@@ -32,6 +33,24 @@ def _convert_pubdate(pubdate):
         offset = datetime.datetime(2001, 1, 1).timestamp()
         return datetime.date.fromtimestamp(offset + pubdate)
     return None
+
+
+class PodcastCache(object):
+    def __init__(self, db):
+        self.db = db
+        self.cache = {}  # indexed by id
+
+    def get(self, podcast_id):
+        podcast = self.cache.get(podcast_id)
+        if not podcast:
+            cursor = self.db.execute('SELECT ZTITLE FROM ZMTPODCAST WHERE Z_PK=?', (podcast_id, ))
+            result = cursor.fetchone()  # tuple
+            if not result:
+                raise KeyError(podcast_id)
+            title, = result
+            podcast = Podcast(title, podcast_id)
+            self.cache[podcast_id] = podcast
+        return podcast
 
 
 class PodcastLibrary(object):
@@ -68,35 +87,32 @@ class PodcastLibrary(object):
         if not library_file.is_file():
             raise LibraryNotFoundException(f"Library file '{library_file}' not found")
         self.db = sqlite3.connect(library_file)
+        self.podcast_cache = PodcastCache(self.db)
 
     def available_podcasts(self):
         """
         Return a list of Podcast objects
         """
-        cursor = self.db.execute('SELECT ZTITLE, Z_PK FROM ZMTPODCAST')
+        cursor = self.db.execute('SELECT Z_PK FROM ZMTPODCAST')
         podcasts = cursor.fetchall()  # list of tuples
-        podcasts = [Podcast(title, podcast_id) for (title, podcast_id) in podcasts]
-        return podcasts
+        return [self.podcast_cache.get(podcast_id) for podcast_id, in podcasts]
 
     def get_podcast_by_title(self, title):
         """
         Return a Podcast object for the given podcast
         """
-        cursor = self.db.execute('SELECT ZTITLE, Z_PK FROM ZMTPODCAST WHERE ZTITLE=?', (title, ))
-        podcast = cursor.fetchone()  # tuple
-        if not podcast:
+        cursor = self.db.execute('SELECT Z_PK FROM ZMTPODCAST WHERE ZTITLE=?', (title, ))
+        result = cursor.fetchone()  # tuple
+        if not result:
             raise KeyError(title)
-        return Podcast(*podcast)
+        podcast_id, = result
+        return self.podcast_cache.get(podcast_id)
 
     def get_podcast_by_id(self, podcast_id):
         """
         Return a Podcast object for the given id
         """
-        cursor = self.db.execute('SELECT ZTITLE, Z_PK FROM ZMTPODCAST WHERE Z_PK=?', (podcast_id, ))
-        podcast = cursor.fetchone()  # tuple
-        if not podcast:
-            raise KeyError(podcast_id)
-        return Podcast(*podcast)
+        return self.podcast_cache.get(podcast_id)
 
     def episode_filepath(self, episode_uuid):
         """
@@ -120,7 +136,8 @@ class PodcastLibrary(object):
                 podcast = self.get_podcast_by_id(podcast_id)
         if not podcast:
             raise ValueError("Podcast not found")
-        cursor = self.db.execute('SELECT ZTITLE, ZPUBDATE, ZPLAYCOUNT, ZUUID FROM ZMTEPISODE WHERE ZPODCAST=? ORDER BY ZPUBDATE', (podcast.podcast_id, ))
+        cursor = self.db.execute('SELECT ZTITLE, ZPUBDATE, ZPLAYCOUNT, ZUUID FROM ZMTEPISODE WHERE ZPODCAST=? ORDER BY ZPUBDATE',
+                                 (podcast.podcast_id, ))
         episodes = cursor.fetchall()  # list of tuples
-        episodes = [Episode(title, _convert_pubdate(pubdate), playcount, self.episode_filepath(uuid), uuid) for (title, pubdate, playcount, uuid) in episodes]
+        episodes = [Episode(title, _convert_pubdate(pubdate), playcount, self.episode_filepath(uuid), uuid, podcast) for (title, pubdate, playcount, uuid) in episodes]
         return episodes
